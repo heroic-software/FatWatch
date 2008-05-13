99895f2ed372ab113ed30e8a1784bd825d5d9acd
Benjamin Ragheb <ben@benzado.com>
2008-05-13 10:05:53 +0000

    Placing subviews in contentView as requested by API; replacing NSString stringWithFormat with NSNumberFormatters instead.

--- Classes/LogTableViewCell.m
+++ Classes/LogTableViewCell.m
@@ -12,35 +12,51 @@
 
 NSString *kLogCellReuseIdentifier = @"LogCell";
 
+static NSNumberFormatter *dayFormatter = nil;
+static NSNumberFormatter *measuredFormatter = nil;
+static NSNumberFormatter *trendFormatter = nil;
+
 @implementation LogTableViewCell
 
 - (id)init
 {
     if ([super initWithFrame:CGRectMake(0, 0, 320, 44) reuseIdentifier:kLogCellReuseIdentifier]) {
+		
+		if (dayFormatter == nil) {
+			dayFormatter = [[NSNumberFormatter alloc] init];
+			measuredFormatter = [[NSNumberFormatter alloc] init];
+			[measuredFormatter setMinimumFractionDigits:1];
+			trendFormatter = [[NSNumberFormatter alloc] init];
+			[trendFormatter setPositivePrefix:@"+"];
+			[trendFormatter setNegativePrefix:@"−"];
+			[trendFormatter setMinimumIntegerDigits:1];
+			[trendFormatter setMinimumFractionDigits:1];
+		}
+		
 		dayLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 44, 44)];
 		dayLabel.textAlignment = UITextAlignmentRight;
 		dayLabel.font = [UIFont systemFontOfSize:20];
 		dayLabel.backgroundColor = [UIColor clearColor];
-		[self addSubview:dayLabel];
+		[self.contentView addSubview:dayLabel];
 		
 		measuredWeightLabel = [[UILabel alloc] initWithFrame:CGRectMake(44, 14, 120, 16)];
 		measuredWeightLabel.textAlignment = UITextAlignmentRight;
 		measuredWeightLabel.font = [UIFont boldSystemFontOfSize:20];
 		measuredWeightLabel.backgroundColor = [UIColor clearColor];
-		[self addSubview:measuredWeightLabel];
+		[self.contentView addSubview:measuredWeightLabel];
 		
 		trendWeightLabel = [[UILabel alloc] initWithFrame:CGRectMake(168, 14, 108, 16)];
 		trendWeightLabel.textAlignment = UITextAlignmentLeft;
 		trendWeightLabel.font = [UIFont systemFontOfSize:20];
 		trendWeightLabel.backgroundColor = [UIColor clearColor];
-		[self addSubview:trendWeightLabel];
+		[self.contentView addSubview:trendWeightLabel];
 				
 		noteLabel = [[UILabel alloc] initWithFrame:CGRectMake(44, 30, 242, 14)];
 		noteLabel.textAlignment = UITextAlignmentCenter;
 		noteLabel.font = [UIFont systemFontOfSize:12];
 		noteLabel.textColor = [UIColor darkGrayColor];
 		noteLabel.backgroundColor = [UIColor clearColor];
-		[self addSubview:noteLabel];
+		[self.contentView addSubview:noteLabel];
     }
     return self;
 }
@@ -56,7 +72,7 @@ NSString *kLogCellReuseIdentifier = @"LogCell";
 
 - (void)updateWithMonthData:(MonthData *)monthData day:(EWDay)day;
 {
-	dayLabel.text = [NSString stringWithFormat:@"%d", day];
+	dayLabel.text = [dayFormatter stringFromNumber:[NSNumber numberWithInt:day]];
 	
 	float measuredWeight = [monthData measuredWeightOnDay:day];
 	if (measuredWeight == 0) {
@@ -64,10 +80,10 @@ NSString *kLogCellReuseIdentifier = @"LogCell";
 		trendWeightLabel.hidden = YES;
 	} else {
 		measuredWeightLabel.hidden = NO;
-		measuredWeightLabel.text = [NSString stringWithFormat:@"%.1f", measuredWeight];
+		measuredWeightLabel.text = [measuredFormatter stringFromNumber:[NSNumber numberWithFloat:measuredWeight]];
 		float weightDiff = measuredWeight - [monthData trendWeightOnDay:day];
 		trendWeightLabel.hidden = NO;
-		trendWeightLabel.text = [NSString stringWithFormat:@"%+.1f", weightDiff];
+		trendWeightLabel.text = [trendFormatter stringFromNumber:[NSNumber numberWithFloat:weightDiff]];
 		trendWeightLabel.textColor = (weightDiff > 0) ? [UIColor redColor] : [UIColor greenColor];
 	}
 	
