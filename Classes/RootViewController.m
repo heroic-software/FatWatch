35d280cb455140d2537dd893f9039042eea59faa
Benjamin Ragheb <ben@benzado.com>
2008-06-03 06:20:14 +0000

    We now fade through black during rotation; this also fixes a layout problem when we add a NavigationController to the log view.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -26,6 +26,7 @@
 	UIView *rootView = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	rootView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
 	rootView.autoresizesSubviews = YES;
+	rootView.backgroundColor = [UIColor blackColor];
 
 	currentViewController = portraitViewController;
 	
@@ -70,48 +71,42 @@
 }
 
 
-- (void)replaceView:(UIViewController *)oldViewController 
-		   withView:(UIViewController *)newViewController duration:(NSTimeInterval)duration {
-	if ([oldViewController.view superview]) {
-		[oldViewController viewWillDisappear:YES];
-		[newViewController viewWillAppear:YES];
-		
-		[oldViewController.view removeFromSuperview];
-		newViewController.view.frame = self.view.bounds;
-		[self.view addSubview:newViewController.view];
-
-		currentViewController = newViewController;
-
-		CATransition *animation = [CATransition animation];
-		[animation setType:kCATransitionFade];
-		[animation setDuration:duration];
-		[[self.view layer] addAnimation:animation forKey:kCATransition];
-	}
-}
-
-
 - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
-	switch (toInterfaceOrientation) {
-		case UIInterfaceOrientationPortrait:
-			[self replaceView:landscapeViewController withView:portraitViewController duration:duration];
-			return;
-		case UIInterfaceOrientationLandscapeLeft:
-		case UIInterfaceOrientationLandscapeRight:
-			[self replaceView:portraitViewController withView:landscapeViewController duration:duration];
-			return;
+	[currentViewController viewWillDisappear:YES];
+	[currentViewController.view removeFromSuperview];
+	if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation)) {
+		currentViewController = portraitViewController;
+	} else {
+		currentViewController = landscapeViewController;
 	}
+	[currentViewController viewWillAppear:YES];
+	CATransition *animation = [CATransition animation];
+	[animation setType:kCATransitionFade];
+	[animation setDuration:duration];
+	[[self.view layer] addAnimation:animation forKey:kCATransition];
 }
 
 
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
-	if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation)) {
-		[portraitViewController viewDidDisappear:YES];
-		[landscapeViewController viewDidAppear:YES];
-	} else {
+	currentViewController.view.frame = self.view.bounds;
+	[self.view addSubview:currentViewController.view];
+	CATransition *animation = [CATransition animation];
+	[animation setType:kCATransitionFade];
+	[animation setDuration:0.2];
+	[animation setDelegate:self];
+	[[self.view layer] addAnimation:animation forKey:kCATransition];
+}
+
+
+- (void)animationDidStop:(CAAnimation *)theAnimation finished:(BOOL)flag {
+	if (UIInterfaceOrientationIsPortrait(self.interfaceOrientation)) {
 		[landscapeViewController viewDidDisappear:YES];
 		[portraitViewController viewDidAppear:YES];
+	} else {
+		[portraitViewController viewDidDisappear:YES];
+		[landscapeViewController viewDidAppear:YES];
 	}
-}
+}	
 
 
 - (void)dealloc {
