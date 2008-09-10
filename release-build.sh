388666f3e9a1e549aabbe50fb6fadd986edc8458
Benjamin Ragheb <ben@benzado.com>
2008-09-10 06:19:36 +0000

    new release build script

---
+++ release-build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+SVN_START_REV=186
+
+RAMDISK_SIZE_MB=10
+ZIP_PATH=$PWD
+
+# CREATE RAM DISK
+
+RAMDISK_SECTORS=$((2048 * $RAMDISK_SIZE_MB))
+RAMDISK_DEVICE=`hdiutil attach -nomount ram://$RAMDISK_SECTORS`
+echo "created ramdisk $RAMDISK_DEVICE"
+RAMDISK_PATH=`mktemp -d /tmp/ramdisk.XXXXXX`
+newfs $RAMDISK_DEVICE
+mount -t ufs $RAMDISK_DEVICE $RAMDISK_PATH
+
+# BUILD
+
+XCODE_TARGET=FatWatch
+
+do_build() {
+	XCODE_CONFIGURATION=$1
+	ZIP_FILE_PATH="$ZIP_PATH/$XCODE_TARGET-$XCODE_CONFIGURATION.zip"
+	
+	xcodebuild \
+			-configuration $XCODE_CONFIGURATION \
+			-target $XCODE_TARGET \
+			build \
+			OBJROOT=$RAMDISK_PATH/Build/Intermediate \
+			SYMROOT=$RAMDISK_PATH/Build/Products
+	cd $RAMDISK_PATH/Build/Products/$XCODE_CONFIGURATION-iphoneos
+	if [ -e $ZIP_FILE_PATH ]; then rm $ZIP_FILE_PATH; fi
+	zip -9 -r $ZIP_FILE_PATH $XCODE_TARGET.app
+	cd -
+}
+
+do_build Distribution
+do_build Release
+
+# CHANGELOG
+
+SVN_BIN=/usr/local/bin/svn
+$SVN_BIN log -r ${SVN_START_REV}:HEAD > $ZIP_PATH/$XCODE_TARGET-Changes.txt
+
+# DISPOSE RAM DISK
+
+umount -f $RAMDISK_DEVICE
+hdiutil detach $RAMDISK_DEVICE
+rmdir $RAMDISK_PATH
